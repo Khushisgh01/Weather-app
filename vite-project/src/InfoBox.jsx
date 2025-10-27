c5d82d20438efa57bae216ac2cafeac0e7fbaf08
unknown <khushi096btcseai24@igdtuw.ac.in>
2025-10-26 23:05:08 -0700

    Finalizing

--- vite-project/src/InfoBox.jsx
+++ vite-project/src/InfoBox.jsx
@@ -2,60 +2,59 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Component } from 'react';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
 
-export default function InfoBox() {
-    const INIT_URL="https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=1000"
-    let info = {
-        city:"Delhi",
-        feelsLike: 24.4,
-        temp: 25.05,
-        tempMin: 25.05,
-        tempMax: 25.05,
-        humidity: 47,
-        weather: "haze",
-    };
+export default function InfoBox({ info }) {
+    const INIT_URL = "https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=1000"
+    const HOT_URL = "https://images.unsplash.com/photo-1447601932606-2b63e2e64331?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3VubnklMjB3ZWF0aGVyfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=600";
+    const COLD_URL = "https://plus.unsplash.com/premium_photo-1670604649107-a0171e5f1bd0?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=600";
+    const RAIN_URL = "https://images.unsplash.com/photo-1558409057-bbe679023136?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbmluZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600";
     return (
         <div className="InfoBox">
-            <h1>WeatherInfo-{info.weather}</h1>
+            <div className='cardContainer'>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={info.humidity > 85 ? RAIN_URL : (info.temp > 15 ? HOT_URL : COLD_URL)}
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {info.city}{
+                                info.humidity > 85 ? <ThunderstormIcon/> : (info.temp > 15 ? <SunnyIcon/> : <AcUnitIcon/>)
+                            }
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
+                            <p>
+                                Temperature : {info.temp}&deg;
+                            </p>
 
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                    sx={{ height: 140 }}
-                    image={INIT_URL}
-                    title="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {info.city}
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
-                        <p>
-                            Temperature : {info.temp}&deg;
-                        </p>
-                        
-                        <p>
-                            The Weather feels like : {info.feelsLike}&deg;
-                        </p>
-                        
-                        <p>
-                            Minimum Temperature : {info.tempMin}&deg;
-                        </p>
-                        
-                        <p>
-                            Maximum Temperature : {info.tempMax}&deg;
-                        </p>
-                        
-                        <p>
-                            Humidity : {info.humidity}
-                        </p>
-                        
-                        <p>
-                            Weather : {info.weather}
-                        </p>
-                    </Typography>
-                </CardContent>
-            </Card>
+                            <p>
+                                Minimum Temperature : {info.tempMin}&deg;
+                            </p>
+
+                            <p>
+                                Maximum Temperature : {info.tempMax}&deg;
+                            </p>
+
+                            <p>
+                                Humidity : {info.humidity}
+                            </p>
+
+                            <p>
+                                Weather : {info.weather}
+                            </p>
+
+                            <p>
+                                The Weather can be described as <i>{info.weather}</i> and it feels like : {info.feelsLike}&deg;
+                            </p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     )
 };
--- vite-project/src/SearchBox.jsx
+++ vite-project/src/SearchBox.jsx
@@ -3,46 +3,60 @@ import Button from '@mui/material/Button';
 import "./SearchBox.css"
 import { useState } from 'react';
 
-export default function SearchBox(){
+export default function SearchBox({updateInfo}){
     let [city,setCity]=useState("");
+    let [error,setError]=useState(false);
 
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
     const API_KEY="e75ab2703f6a42c15727f06578b4b828";
     //this getWeatherInfo should be a async function as we are doing an API call here 
     // API calls are generally asynchronousfunctions
     let getWeatherInfo=async()=>{
-        let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-        let jsonResponse=await response.json();
-        let result={
-            city:city,
-            temp: jsonResponse.main.temp,
-            tempMin:jsonResponse.main.temp_min,
-            tempMax:jsonResponse.main.humidity,
-            feelsLike:jsonResponse.main.feels_like,
-            weather:jsonResponse.weather[0].description
-        };
-        console.log(result);
+        try{
+            let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            let jsonResponse=await response.json();
+            let result={
+                city:city,
+                temp: jsonResponse.main.temp,
+                tempMin:jsonResponse.main.temp_min,
+                tempMax:jsonResponse.main.humidity,
+                feelsLike:jsonResponse.main.feels_like,
+                weather:jsonResponse.weather[0].description,
+                humidity:jsonResponse.main.humidity,
+            };
+            console.log(result);
+            return result;
+        }catch(err){
+            throw err;
+        }
+        
     };
     
 
 let handleChange=(event)=>{
     setCity(event.target.value);
 }
-let handleSubmit=(event)=>{
-    event.preventDefault();
-    console.log(city);
-    setCity("");
-    getWeatherInfo();
+let handleSubmit=async(event)=>{
+    try{
+        event.preventDefault();
+        console.log(city);
+        setCity("");
+        let newInfo=await getWeatherInfo();
+        updateInfo(newInfo);
+    }catch(err){
+        setError(true);
+    }
+    
 }
     return (
         <div className='SearchBox'>
-            <h3> Search for the Weather </h3>
             <form onSubmit={handleSubmit}>
                 <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
                 <br></br><br></br>
                 <Button variant="contained" type='submit'>
                     SEARCH
                 </Button>
+                {error && <p style={{color:"red"}}>No such place exists!</p>}
             </form>
         </div>
     )
